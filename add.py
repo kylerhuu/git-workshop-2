f1a85ff76f81e0e8915bd78afa44cde49334ff21
Kyler Hu <kylerhu1549@gmail.com>
2026-01-30 23:30:06 -0800

    made changes to txt file and python code

--- add.py
+++ add.py
@@ -1,4 +1,17 @@
+import random
+
 def add(x, y):
   return x + y
 
+def sayhi(line:str, num:int):
+  for i in range(num):
+    print(line)
+
 print(add(6, 7))
+
+line = "ur gonna crash out"
+num = random.randrange(1,11)
+
+sayhi(line, num)
+print("the number was", num)
+print("and the line was", line)
